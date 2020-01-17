5553effbddda70e7f80c3bdeb022cffafa4f40fa
wardahmad <whitelight43@hotmail.com>
2020-01-17 21:45:01 +0300

    Updates

---
+++ precache-manifest.c8d6e494e50c3d6097a01476c11d750f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909adf50564339e86d2a9e52c3b40e72",
+    "url": "/wardahmad/dentistWebsite/index.html"
+  },
+  {
+    "revision": "7f73d603b8169d1ed6d2",
+    "url": "/wardahmad/dentistWebsite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46a1e01ac2b75913409a",
+    "url": "/wardahmad/dentistWebsite/static/js/2.cedec3f8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/wardahmad/dentistWebsite/static/js/2.cedec3f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f73d603b8169d1ed6d2",
+    "url": "/wardahmad/dentistWebsite/static/js/main.e49cef25.chunk.js"
+  },
+  {
+    "revision": "3e131b57cc1b09b965d2",
+    "url": "/wardahmad/dentistWebsite/static/js/runtime-main.a1b0d161.js"
+  }
+]);
